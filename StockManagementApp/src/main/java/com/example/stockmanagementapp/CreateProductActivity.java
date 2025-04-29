a0b65935abfaed62537831e0814e9e7ba3065f4e
vetimesss <vectimesss@gmail.com>
2025-04-29 14:20:13 +0100

    Add StockManagementApp/src/main/java/com/example/stockmanagementapp/CreateProductActivity.java 1745932812866

---
+++ StockManagementApp/src/main/java/com/example/stockmanagementapp/CreateProductActivity.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateProductActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_product);
+        // TODO: Implement Create Product functionality
+    }
+}
