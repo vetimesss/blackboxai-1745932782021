725c1e6a6306924165b6f2eff38aae5f7caa03cf
vetimesss <vectimesss@gmail.com>
2025-04-29 14:20:11 +0100

    Add StockManagementApp/src/main/java/com/example/stockmanagementapp/AddSupplierActivity.java 1745932811506

---
+++ StockManagementApp/src/main/java/com/example/stockmanagementapp/AddSupplierActivity.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddSupplierActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_supplier);
+        // TODO: Implement Add Supplier functionality
+    }
+}
