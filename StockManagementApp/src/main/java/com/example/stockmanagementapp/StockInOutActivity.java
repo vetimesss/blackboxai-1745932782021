e6830e09071a89407e2074a2e247c56e37cbeade
vetimesss <vectimesss@gmail.com>
2025-04-29 14:20:07 +0100

    Add StockManagementApp/src/main/java/com/example/stockmanagementapp/StockInOutActivity.java 1745932807958

---
+++ StockManagementApp/src/main/java/com/example/stockmanagementapp/StockInOutActivity.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StockInOutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_in_out);
+        // TODO: Implement Stock In/Out functionality
+    }
+}
