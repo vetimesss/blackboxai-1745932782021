0575593d61477b782c47007532a541abb92b9c52
vetimesss <vectimesss@gmail.com>
2025-04-29 14:20:09 +0100

    Add StockManagementApp/src/main/java/com/example/stockmanagementapp/NewOrderActivity.java 1745932810015

---
+++ StockManagementApp/src/main/java/com/example/stockmanagementapp/NewOrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.stockmanagementapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewOrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+        // TODO: Implement New Order functionality
+    }
+}
